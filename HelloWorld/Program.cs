ea4093f53903fa7a943be230ab23ddbb30d771b1
dimon080204 <batyshnyi.dmytro.54@gmail.com>
2025-09-04 13:28:17 +0300

    Add GreetUser function to Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GreetUser("Dmytro");
+        }
+
+        static void GreetUser(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
