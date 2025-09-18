cc5800f9a868e03c64ab064a916ffb8ce2065e29
dimon080204 <105213272+dimon080204@users.noreply.github.com>
2025-09-18 13:06:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GreetUser("Dmytro");
+        }
+
+        static void GreetUser(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
